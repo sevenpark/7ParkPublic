e31f16dd06229f7008dfa22b79144ffa60ea6f25
Alexander Arbatskiy <arbatskiy@7parkdata.com>
2019-12-03 15:38:33 -0800

    Drug NER documentation

---
+++ aws_marketplace/using_model_packages/drug_ner/data/samples.jl
@@ -0,0 +1,5 @@
+{"id": 0, "instance": "Subsequently, eligible patients who have not experienced disease progression at week 24 will continue in a maintenance phase where a single dose of ipilimumab will be administered once every 12 weeks until disease progression."}
+{"id": 1, "instance": "The investigational new drug （ IND ） application for IBI306 was approved by the National Medical Products Administration (NMPA) in September 2017."}
+{"id": 2, "instance": "In June 2005, UCB announced significant positive results for the two pivotal phase III trials (PRECiSE 1 and 2) of CIMZIATM in the induction and maintenance of clinical response in moderate to severe active Crohn's disease."}
+{"id": 3, "instance": "Carefully weigh the risks and benefits of treatment with OTEZLA for patients with a history of depression and/or suicidal thoughts/behavior, or in patients who develop such symptoms while on OTEZLA."}
+{"id": 4, "instance": "In contrast, pre-clinical data indicate that PRTX-100 may have the potential to treat ITP by reducing the immune-mediated destruction of the platelets through immunomodulatory processes that don ’ t lead to immunosuppression."}
