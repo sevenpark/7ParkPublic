45a3fbec1273ff648606be3b3bdf9d7c9123866a
EC2 Default User <ec2-user@ip-172-16-66-40.ec2.internal>
2019-12-03 06:33:37 +0000

    Fix chat description

--- aws_marketplace/using_model_packages/chat_and_informal_text_ner/data/samples.jl
+++ aws_marketplace/using_model_packages/chat_and_informal_text_ner/data/samples.jl
@@ -1,5 +1,5 @@
 {"id": 0, "instance": "Just checked, and the nflx data looks pretty postive"}
-{"id": 1, "instance": "we should definitely boost aapl weight"}
+{"id": 1, "instance": "we should definitely boost Apple weight"}
 {"id": 2, "instance": "Uber just published its lastest revenue number"}
 {"id": 3, "instance": "The report of tsla will be released today"}
-{"id": 4, "instance": "atvi got trapped and people just traded it""}
+{"id": 4, "instance": "atvi got trapped and people just traded it"}
