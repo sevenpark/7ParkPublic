c585ad2cbff391b739352debed46b489ae86b7f7
Kaiying Chen <ckykokoko@gmail.com>
2020-02-14 15:19:26 -0500

    update algorithm arns

--- aws_marketplace/using_algorithm/stopword_algorithm/src/algorithm_arns.py
+++ aws_marketplace/using_algorithm/stopword_algorithm/src/algorithm_arns.py
@@ -3,6 +3,6 @@ class AlgorithmArnProvider:
     @staticmethod
     def get_algorithm_arn(current_region):
         mapping = {
-            "us-east-2": "arn:aws:sagemaker:us-east-2:084888172679:algorithm/stopword-2020-02-13-2",
+            "us-east-2": "arn:aws:sagemaker:us-east-2:084888172679:algorithm/stopword-2020-02-14-5",
         }
         return mapping[current_region]
